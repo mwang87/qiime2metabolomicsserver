77113f73a476a231efb3bf67e12d2fe0be6ff8c8
Mingxun Wang <mwang87@gmail.com>
2019-03-04 18:46:47 -0800

    updating manifest manipulation

--- test/create_manifest.py
+++ test/create_manifest.py
@@ -2,18 +2,21 @@ import pandas as pd
 import os
 import sys
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2]
+input_metadata_filename = sys.argv[1]
+input_quantification_table = sys.argv[2]
+output_filename = sys.argv[3]
 
-df = pd.read_csv(input_filename, sep="\t")
+df_metadata = pd.read_csv(input_metadata_filename, sep="\t")
+df_quantification = pd.read_csv(input_quantification_table, sep="\t")
 
-if "sample_name" in df:
+if "sample_name" in df_metadata:
     manifest_df = df["sample_name", "filename"]
 else:
-    #manifest_df = df["filename", "ATTRIBUTE_instrument"]
-    #manifest_df = df.loc[:, ['filename']]
     manifest_df = pd.DataFrame()
-    manifest_df["sample_name"] = df["filename"]
-    manifest_df["filepath"] = df["filename"]
+    manifest_df["sample_name"] = df_metadata["filename"]
+    manifest_df["filepath"] = df_metadata["filename"]
+
+"""Checking if the set of filenames are fully covered, if not then we'll provide a place holder"""
+#TODO: FINISH
 
 manifest_df.to_csv(output_filename, index=False, sep=",")
